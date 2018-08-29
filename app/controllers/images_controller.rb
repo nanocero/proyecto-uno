867cabfec0869f8fb3ad2f3348621a8bf1d31978
nanocero <orlando.cero@gmail.com>
2018-08-28 21:39:54 -0500

    final rails

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -31,7 +31,7 @@ class ImagesController < ApplicationController
 
     private
     def images_params
-        params.require(:image).permit(:description)
+        params.require(:image).permit :description, :picture
     end 
     def set_image
         @image = Image.find params[:id]
